12c1179ee829a01eec4cb444705433c016b97d3f
offwork <chomunist@gmail.com>
2025-11-25 20:12:43 +0300

    test(document): add Fragment creation with nodes test [RED]

--- packages/core/document/src/lib/entities/Fragment.spec.ts
+++ packages/core/document/src/lib/entities/Fragment.spec.ts
@@ -1,13 +1,24 @@
+import { NodeType } from '../value-objects/NodeType';
 import { Fragment } from './Fragment';
 import { Node } from './Node';
 
 describe('Fragment', () => {
   describe('creation', () => {
-    test('creates empty fragment', () => {
+    it('creates empty fragment', () => {
       const fragment = Fragment.empty<Node>();
 
       expect(fragment).toBeInstanceOf(Fragment);
       expect(fragment.size).toBe(0);
+    });
+
+    it('creates fragment with nodes', () => {
+      const node1 = new Node(new NodeType('paragraph'), { text: 'Hello' });
+      const node2 = new Node(new NodeType('paragraph'), { text: 'World' });
+
+      const fragment = Fragment.from([node1, node2]);
+
+      expect(fragment).toBeInstanceOf(Fragment);
+      expect(fragment.size).toBe(2);
     });
   });
 });
