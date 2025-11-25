4250e87978c29263ae41feb8a7bbecee1d37fb9f
offwork <chomunist@gmail.com>
2025-11-25 12:56:19 +0300

    feat(document): implement Fragment empty creation [GREEN]

--- packages/core/document/src/lib/entities/Fragment.spec.ts
+++ packages/core/document/src/lib/entities/Fragment.spec.ts
@@ -1,10 +1,12 @@
-import { Fragment } from "./Fragment";
+import { Fragment } from './Fragment';
+import { Node } from './Node';
 
 describe('Fragment', () => {
   describe('creation', () => {
-    it('creates empty fragment', () => {
-      const fragment = new Fragment();
+    test('creates empty fragment', () => {
+      const fragment = Fragment.empty<Node>();
 
+      expect(fragment).toBeInstanceOf(Fragment);
       expect(fragment.size).toBe(0);
     });
   });
--- packages/core/document/src/lib/entities/Fragment.ts
+++ packages/core/document/src/lib/entities/Fragment.ts
@@ -1,1 +1,15 @@
-export class Fragment {}
+export class Fragment<TNode> {
+  private readonly content: readonly TNode[];
+
+  private constructor(content: readonly TNode[]) {
+    this.content = content;
+  }
+
+  static empty<TNode>(): Fragment<TNode> {
+    return new Fragment<TNode>([]);
+  }
+
+  get size(): number {
+    return this.content.length;
+  }
+}
