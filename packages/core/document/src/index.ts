f0d3ee8af48671d8e5145a9d600e3d5344ad19a1
offwork <chomunist@gmail.com>
2025-11-26 17:17:27 +0300

    test(document): add Mark construction test [RED]

--- packages/core/document/src/index.ts
+++ packages/core/document/src/index.ts
@@ -6,3 +6,4 @@ export { Fragment } from './lib/entities/Fragment';
 
 // Value Objects
 export { NodeType } from './lib/value-objects/NodeType';
+export { Mark } from './lib/value-objects/Mark';
---
+++ packages/core/document/src/lib/value-objects/Mark.spec.ts
@@ -0,0 +1,15 @@
+import { Mark } from "./Mark";
+
+describe('Mark Value Object', () => {
+  describe('Construction', () => {
+    it('should create mark with type and attrs', () => {
+      const type = 'bold';
+      const attrs = { color: 'purple' };
+
+      const mark = new Mark(type, attrs);
+
+      expect(mark.type).toBe('bold');
+      expect(mark.attrs).toEqual({ color: 'purple' });
+    });
+  });
+});
