e13936b8fbe71328c145e7d9cc1f0bb71ef187df
offwork <chomunist@gmail.com>
2025-11-26 23:52:07 +0300

    feat(document): implement type validation [GREEN]

--- packages/core/document/src/lib/value-objects/Mark.spec.ts
+++ packages/core/document/src/lib/value-objects/Mark.spec.ts
@@ -15,5 +15,17 @@ describe('Mark Value Object', () => {
     it('should throw when type is null', () => {
       expect(() => new Mark(null as never, {})).toThrow('Type must be a string');
     });
+
+    it('should throw when type is undefined', () => {
+      expect(() => new Mark(undefined as never, {})).toThrow('Type must be a string');
+    });
+
+    it('should throw when attrs is null', () => {
+      expect(() => new Mark('bold', null as never)).toThrow('Attrs must be an object');
+    });
+
+    it('should throw when attrs is undefined', () => {
+      expect(() => new Mark('bold', undefined as never)).toThrow('Attrs must be an object');
+    });
   });
 });
--- packages/core/document/src/lib/value-objects/Mark.ts
+++ packages/core/document/src/lib/value-objects/Mark.ts
@@ -2,6 +2,10 @@ export class Mark<TAttrs = Record<string, unknown>> {
   readonly type: string;
   readonly attrs: TAttrs;
   constructor(type: string, attrs: TAttrs) {
+    if (typeof type !== 'string') throw new Error('Type must be a string');
+    if (typeof attrs !== 'object' || attrs === null)
+      throw new Error('Attrs must be an object');
+
     this.attrs = { ...attrs } as TAttrs;
     this.type = type;
   }
