78e610a7dd89ee0cd9f1662f0b84ebd07b09127b
offwork <chomunist@gmail.com>
2025-11-26 22:26:54 +0300

    feat(document): implement Mark constructor [GREEN]

--- packages/core/document/src/lib/value-objects/Mark.ts
+++ packages/core/document/src/lib/value-objects/Mark.ts
@@ -1,3 +1,8 @@
 export class Mark {
-
+  type: string;
+  attrs: Record<string, unknown>;
+  constructor(type: string, attrs: Record<string, unknown>) {
+    this.attrs = attrs;
+    this.type = type;
+  }
 }
