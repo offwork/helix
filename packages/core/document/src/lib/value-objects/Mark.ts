255a5d4a96511205834ae45d8a72dd80cef054be
offwork <chomunist@gmail.com>
2025-11-27 14:34:24 +0300

    refactor(document): replace JSON.stringify with manual shallow equality [REFACTOR]

--- packages/core/document/src/lib/value-objects/Mark.ts
+++ packages/core/document/src/lib/value-objects/Mark.ts
@@ -10,7 +10,43 @@ export class Mark<TAttrs = Record<string, unknown>> {
     this.type = type;
   }
 
+  /**
+   * Compares this mark with another mark for equality.
+   *
+   * Two marks are considered equal if they have the same type and
+   * equivalent attributes (shallow comparison).
+   *
+   * @param other - The mark to compare with
+   * @returns true if marks are equal, false otherwise
+   *
+   * @remarks
+   * Currently supports flat attributes only. Nested objects are compared
+   * by reference, not deep equality. Deep equality support is planned for
+   * future milestones.
+   *
+   * @example
+   * ```typescript
+   * const mark1 = new Mark('bold', { color: 'red' });
+   * const mark2 = new Mark('bold', { color: 'red' });
+   * mark1.equals(mark2); // true
+   * ```
+   */
   equals(other: Mark<TAttrs>): boolean {
-    return other.type === this.type && JSON.stringify(this.attrs) === JSON.stringify(other.attrs);
+    if (other.type !== this.type) {
+      return false;
+    }
+
+    const thisKeys = Object.keys(this.attrs as object);
+    const otherKeys = Object.keys(other.attrs as object);
+
+    if (thisKeys.length !== otherKeys.length) {
+      return false;
+    }
+
+    return thisKeys.every((key) => {
+      return (
+        this.attrs[key as keyof TAttrs] === other.attrs[key as keyof TAttrs]
+      );
+    });
   }
 }
