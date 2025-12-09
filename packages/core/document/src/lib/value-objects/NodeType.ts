0ef395a0c06678cd32d238ef7cefabbab8ec11ee
offwork <chomunist@gmail.com>
2025-12-09 20:09:48 +0300

    feat(document): implement NodeType constructor [GREEN]

--- packages/core/document/src/lib/value-objects/NodeType.ts
+++ packages/core/document/src/lib/value-objects/NodeType.ts
@@ -1,11 +1,17 @@
+import { NodeSpec } from '../interfaces/SchemaSpec';
+
 export class NodeType {
   readonly name: string;
+  readonly schema: unknown;
+  readonly spec: NodeSpec;
 
-  constructor(name: string) {
+  constructor(name: string, schema: unknown, spec: NodeSpec) {
     if (!name || name.trim() === '') {
       throw new Error('NodeType name cannot be empty');
     }
     this.name = name;
+    this.schema = schema;
+    this.spec = spec;
   }
 
   equals(other: NodeType): boolean {
