eac856d72b0fc201bdb942bdb94018c176a0a764
kerem <offwork@users.noreply.github.com>
2026-06-23 14:51:02 +0300

    feat(document): Add NodeSpec and MarkSpec optional fields (#113)
    - Add defining, definingAsContext, definingForContent, draggable,
      selectable, toDebugString to NodeSpec
    - Add code, spanning to MarkSpec
    
    Closes #90, #91, #92, #93, #94, #95, #96, #97

--- packages/core/document/src/lib/interfaces/SchemaSpec.ts
+++ packages/core/document/src/lib/interfaces/SchemaSpec.ts
@@ -11,20 +11,28 @@ export interface NodeSpec {
   attrs?: Record<string, AttributeSpec>;
   code?: boolean;
   content?: string;
+  defining?: boolean;
+  definingAsContext?: boolean;
+  definingForContent?: boolean;
+  draggable?: boolean;
   group?: string;
   inline?: boolean;
   isolating?: boolean;
   leafText?: (node: INode) => string
   linebreakReplacement?: boolean;
   marks?: string;
+  selectable?: boolean;
+  toDebugString?: (node: INode) => string;
   whitespace?: 'normal' | 'pre';
 }
 
 export interface MarkSpec {
   attrs?: Record<string, AttributeSpec>;
+  code?: boolean;
   excludes?: string;
   group?: string;
   inclusive?: boolean;
+  spanning?: boolean;
 }
 
 export interface AttributeSpec {
